e4d61bff156b5d1a682fba402634481fe6aca1e9
tojocyriaque <ramarosoncyriaque@gmail.com>
2025-09-23 14:32:32 +0300

    ignoring changes in main.rs now

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,25 @@ mod testing;
 fn main() {
     // TESTING MODELS
     // ==================== MODEL TESTING CONSTANTS ==========================
-    // const MODEL_V3: &str = "models/fn_model_v3/fn_model_v3_E9.bin";
-    // const MODEL_V5: &str = "models/fn_model_v5/fn_model_v5_E20.bin";
-    // const TESTS_FILE: &str = "datasets/tests.txt";
-    // test_model(MODEL_V3, TESTS_FILE);
-    // test_model(MODEL_V5, TESTS_FILE);
+    // const TESTS_FILE: &str = "datasets/balanced_tests.txt";
+    // test_model("models/fn_model_v5/fn_model_v5_E20.bin", TESTS_FILE);
+    // test_model("models/fn_model_v6/fn_model_v6_E20.bin", TESTS_FILE);
     // =======================================================================
 
     // CREATING NEW MODELS
     // ==================== TRAINING CONSTANTS ===============================
-    const LEARNING_RATE: f64 = 0.1;
-    const EPOCHS: usize = 20;
-    const TRAIN_FILE: &str = "datasets/trainings.txt";
-    // this is the directory where you model will be registered
+    // const LEARNING_RATE: f64 = 0.1;
+    // const EPOCHS: usize = 20;
+    // const TRAIN_FILE: &str = "datasets/trainings.txt";
+    // // this is the directory where you model will be registered
 
-    const MODELS_DIR: &str = "models"; // Directory where models will be saved
-    //This is the identification of your model type
-    const MODEL_NAME: &str = "fn_model_v5"; // Each model will be saved for every epochs of the training
-    const INPUT_SIZE: usize = 46;
-    // =======================================================================
+    // const MODELS_DIR: &str = "models"; // Directory where models will be saved
+    // //This is the identification of your model type
+    // const MODEL_NAME: &str = "fn_model_v6"; // Each model will be saved for every epochs of the training
+    // const INPUT_SIZE: usize = 46;
+    // // =======================================================================
 
-    let layer_sizes: Vec<usize> = vec![64, 64, 18];
-    let mut nn: NeuralNetwork = NeuralNetwork::new(&layer_sizes, INPUT_SIZE, LEARNING_RATE);
-    train_model(&mut nn, TRAIN_FILE, MODELS_DIR, MODEL_NAME, EPOCHS);
+    // let layer_sizes: Vec<usize> = vec![64, 64, 18];
+    // let mut nn: NeuralNetwork = NeuralNetwork::new(&layer_sizes, INPUT_SIZE, LEARNING_RATE);
+    // train_model(&mut nn, MODELS_DIR, TRAIN_FILE, MODEL_NAME, EPOCHS);
 }
